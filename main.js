3165d9f22ac7cebd28e934073cbf8b4f8f79d46a
Danilo <96477921+danilojezernik@users.noreply.github.com>
2022-02-11 01:44:37 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let nazivText = document.getElementById("nazivText");
 let dodanNaziv = document.getElementById("dodanNaziv");
-let pritisnjeno = false;
-
+//konec naziva računa
+let pritisnjeno = false; //v primeru vnosa
 //konec naziva računa
 
 
@@ -9,8 +9,25 @@ function dodajNaziv() {
     let textNaziv = nazivText.value;
     let naziv = document.createElement("div");
     if (!pritisnjeno) {
-        pritisnjeno = true;
-        naziv.innerHTML = `<p class="fw-bold">${textNaziv}</p>`;
+        pritisnjeno = false;
+        naziv.innerHTML = `<span class="fw-bold">${textNaziv}</span><button style="border: 0; background: transparent;" onclick="izbrisiSebe(dodanNaziv)"><img src="https://img.icons8.com/ios-glyphs/30/000000/xbox-x.png"/></button>`;
         dodanNaziv.appendChild(naziv);
-    }
+        document.getElementById("dodanNaziv").value = "";
+        reset();
+    } else {
+        let naziv2 = document.createElement("div");
+        naziv2.innerHTML = `<span class="fw-bold">Prosim dodajte text</span>`;
+        dodanNaziv.appendChild(naziv2);
+        setTimeout(() => dodanNaziv.remove(naziv2), 2000);
+
+    } let gumbTiska = document.getElementById("printpagebutton");
+}
+
+function reset() {
+    document.getElementById("nazivText").value = "";
+    document.getElementById("dodanNaziv").value = "";
+}
+
+function izbrisiSebe(dodanNaziv) {
+    dodanNaziv.remove();
 }
