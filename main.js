54ca83c083cbb354117164a366fd653191da8ff3
Danilo <96477921+danilojezernik@users.noreply.github.com>
2022-02-12 21:13:18 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -24,17 +24,19 @@ let minuteDodaj = document.getElementById("minuteDodaj");
 let urnaPostavka = document.getElementById("urnaPostavka");
 let sestevek = document.getElementById("sestevek");
 let skupaj = document.getElementById("skupaj");
+let skupaj2 = document.getElementById("skupaj2");
 let dodajDatumVpisa = document.getElementById("dodajDatumVpisa");
 //konec enojni znesek
 
 let datumDanes = document.getElementById("datumDanes");
 
 function dodajNaziv() {
-
     let textNaziv = nazivText.value;
-    let naziv = document.createElement("div");
+    let naziv = document.createElement("div").appendChild(document.createElement(`dodanNaziv`));
         naziv.innerHTML = `
-
+                            <button style="border: 0; background: transparent;" onclick="odstrani(${spisek.length})">
+                            <img src="https://img.icons8.com/ios-glyphs/30/000000/xbox-x.png"/>
+                            </button>
                             <span class="fw-bold">${textNaziv}</span>`;
 
 //<button style="border: 0; background: transparent;" onclick="izbrisiSebe(dodanNaziv)">
@@ -48,9 +50,16 @@ function dodajNaziv() {
 function dodajStRacuna() {
     let textStRacuna = nazivStRacuna.value;
     let stRacun = document.createElement("div");
-    stRacun.innerHTML = `<span class="fw-bold">Račun: #${textStRacuna}</span><br>`;
+    stRacun.innerHTML = `
+                            <button style="border: 0; background: transparent;" onclick="odstrani(${spisek.length})">
+                            <img src="https://img.icons8.com/ios-glyphs/30/000000/xbox-x.png"/>
+                            </button>
+
+                        <span class="fw-bold">Račun: #${textStRacuna}</span><br>`;
+
     stevilkaRacuna.appendChild(stRacun);
     reset();
+    spisek.push(stRacun);
 
     let today = new Date();
     let dd = String(today.getDate()).padStart(2, '0');
@@ -61,9 +70,14 @@ function dodajStRacuna() {
     let danasnji = document.createElement("div");
     danasnji.innerHTML = `
     
-                        Račun izdan: ${today}`;
+                            <button style="border: 0; background: transparent;" onclick="odstrani(${spisek.length})">
+                            <img src="https://img.icons8.com/ios-glyphs/30/000000/xbox-x.png"/>
+                            </button>
+
+                            Račun izdan: ${today}`;
 
     datumDanes.appendChild(danasnji);
+    spisek.push(danasnji);
 }
 
 function dodajZapadlost() {
@@ -81,13 +95,19 @@ function vnesiPodatke() {
     let mobitel = dodajSvojoStevilko.value;
     let kontakti = document.createElement("div");
     kontakti.innerHTML = `
-                            Račun za:<br>
+
+                            Račun za:
+                            <button style="border: 0; background: transparent;" onclick="odstrani(${spisek.length})">
+                            <img src="https://img.icons8.com/ios-glyphs/30/000000/xbox-x.png"/>
+                            </button>
+                            <br>
                             <span class="fw-bold">${ime} ${priimek}</span><br>
                             <span class="fw-bold">${email}</span><br>
                             <span class="fw-bold">telefon: ${mobitel}</span>`;
 
     racunZa.appendChild(kontakti);
     reset();
+    spisek.push(kontakti);
 }
 
 function dodajZnesekEnotni() {
@@ -96,25 +116,27 @@ function dodajZnesekEnotni() {
     let postavka = parseFloat(urnaPostavka.value);
     let sestevek1 = parseFloat(sestevek.value);
     let vsota = parseFloat(skupaj.value);
+    let vsota2 = parseFloat(skupaj2.value);
     let rezultat;
     rezultat = min * postavka;
     vsota += rezultat;
     sestevek1 += rezultat;
     let elementi = document.createElement("element");
     elementi.innerHTML = `
-                            <div class="d-flex bd-highlight border border-1 pt-2">
-                            <div class="p-4 w-100 bd-highlight">
+                            <div class="d-flex bd-highlight border border-1 mt-2">
                             <button style="border: 0; background: transparent;" onclick="odstrani(${spisek.length})">
                             <img src="https://img.icons8.com/ios-glyphs/30/000000/xbox-x.png"/></button>
+                            <div class="p-4 w-100 bd-highlight">
                             <b>Inštrukcije programiranja</b><br>
-                            <span id="minute">120 x0,32 EUR</span> | <span id="danStorite">${datum}</span><br>
+                            <span id="minute">${min} x${postavka}</span> | <span id="danStorite">${datum}</span><br>
                             </div>
                             <div class="p-4 flex-shrink-1 bd-highlight mt-auto">
-                            <span class="fw-bold" id="enojniZnesek">${rezultat}</span>
+                            <span class="fw-bold" id="enojniZnesek">${rezultat}€</span>
                             </div>
                             </div>`;
 
     element.appendChild(elementi);
+    spisek.push(elementi);
 
     let skupni1 = document.createElement("sestevek");
     skupni1.innerHTML = `
@@ -128,7 +150,15 @@ function dodajZnesekEnotni() {
                             ${vsota}€`;
 
     skupaj.appendChild(skupni2);
-    spisek.push(elementi);
+
+    let skupni3 = document.createElement("skupaj");
+    skupni3.innerHTML = `
+                             Za plačilo:
+                            <span class="display-6">${vsota2}€</span>
+                            
+                        `;
+    skupaj2.appendChild(skupni3);
+reset();
 }
 
 //izbriše en vnos
@@ -148,6 +178,9 @@ function reset() {
     document.getElementById("dodajSvojeIme").value = null;
     document.getElementById("dodajSvojPriimek").value = null;
     document.getElementById("dodajSvojEmail").value = null;
+    document.getElementById("minuteDodaj").value = null;
+    document.getElementById("urnaPostavka").value = null;
+    document.getElementById("dodajDatumVpisa").value = null;
 }
 
 //izbriše vse vnose
